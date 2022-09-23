145884cd50409510c8f582d65ca555f3b4f78ff6
qkrtpgh5033 <qkrtpgh5033@gmail.com>
2022-09-23 14:35:32 +0900

    feat : withParam Job 추가

--- src/main/java/com/ll/exam/app_2022_09_22/App20220922Application.java
+++ src/main/java/com/ll/exam/app_2022_09_22/App20220922Application.java
@@ -5,11 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableBatchProcessing // 얘를 사용해야 배치가 됨. ( 작업을 위한 DB 테이블이 생성 )
+@EnableBatchProcessing
 public class App20220922Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(App20220922Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(App20220922Application.class, args);
+    }
 
 }
