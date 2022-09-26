06c48e741c3b99d7279d0bff3e497a5587620f76
qkrtpgh5033 <qkrtpgh5033@gmail.com>
2022-09-26 13:24:17 +0900

    feat : cart, product, productOption 엔티티 및 MVC 추가

---
+++ src/main/java/com/ll/exam/app_2022_09_22/app/cart/service/CartService.java
@@ -0,0 +1,39 @@
+package com.ll.exam.app_2022_09_22.app.cart.service;
+
+import com.ll.exam.app_2022_09_22.app.cart.entity.CartItem;
+import com.ll.exam.app_2022_09_22.app.cart.repository.CartItemRepository;
+import com.ll.exam.app_2022_09_22.app.member.entity.Member;
+import com.ll.exam.app_2022_09_22.app.product.entity.ProductOption;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CartService {
+    private final CartItemRepository cartItemRepository;
+
+    public void addItem(Member member, ProductOption productOption, int quantity) {
+        CartItem cartItem = null;
+
+
+
+        Optional<CartItem> optOldCartItem = cartItemRepository.findByMemberIdAndProductOptionId(member.getId(), productOption.getId());
+        if(optOldCartItem.isPresent()){
+            cartItem = optOldCartItem.get();
+            cartItem.setQuantity(cartItem.getQuantity() + quantity);
+            cartItemRepository.save(cartItem);
+            return;
+        }
+
+        cartItem = CartItem.builder()
+                .member(member)
+                .productOption(productOption)
+                .quantity(quantity)
+                .build();
+
+        cartItemRepository.save(cartItem);
+    }
+}
--- src/main/java/com/ll/exam/app_2022_09_22/app/member/service/MemberService.java
+++ src/main/java/com/ll/exam/app_2022_09_22/app/member/service/MemberService.java
@@ -8,17 +8,18 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class MemberService {
-
     private final MemberRepository memberRepository;
 
-    public Member join(String username, String password, String email){
-        Member member = Member.builder()
+    public Member join(String username, String password, String email) {
+        Member member = Member
+                .builder()
                 .username(username)
                 .password(password)
                 .email(email)
                 .build();
 
         memberRepository.save(member);
+
         return member;
     }
 }
